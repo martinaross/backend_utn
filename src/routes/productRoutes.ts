33c8237b1222afb18f0bf18f8a1f0bd100e99ad4
gabrielalberini <gabialberini733@gmail.com>
2025-11-06 17:40:53 -0300

    add: products input validators

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -8,7 +8,7 @@ import ProductController from "../controllers/productController"
 const productRouter = Router()
 
 // TODAS LAS PETICIONES QUE LLEGAN AL PRODUCTROUTER EMPIEZAN CON
-// http://localhost:3000/products
+// POST http://localhost:3000/products/
 
 productRouter.get("/", ProductController.getAllProducts)
 productRouter.get("/:id", ProductController.getProduct)
